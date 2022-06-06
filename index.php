ab05ac5c92e9f574bb76e7e4584096b9852dfca9
GioCri01 <giovanni.crispino94@icloud.com>
2022-06-06 16:07:10 +0200

    Svolgimento esercizio

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-$text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci vero expedita natus porro optio blanditiis nisi doloribus necessitatibus possimus nihil! Saepe, cumque aut iure ullam nostrum ratione reiciendis. Vero, ut. Quidem aliquid, distinctio ut officiis inventore et recusandae nisi accusamus modi dolor consectetur laborum. Aliquam facere adipisci qui numquam quis ducimus quidem deleniti? Hic quibusdam quidem error suscipit pariatur provident quia nisi eum amet repellendus ad, laudantium inventore reiciendis temporibus numquam impedit. Iure, nihil eos maiores recusandae nam sapiente labore at officiis deserunt eveniet consequatur, consectetur veritatis voluptate veniam unde sunt praesentium placeat laborum doloribus quo. Quisquam exercitationem ipsam et officiis veniam tempora porro, blanditiis eveniet tenetur consectetur? Corrupti unde quae porro dolor at harum officiis obcaecati accusamus doloremque, commodi rem tenetur dignissimos omnis, voluptatem modi, magnam numquam. Vitae iste dolores enim perferendis magnam itaque repellat mollitia ipsam ratione ipsa vel numquam, facere porro quasi odit commodi animi minus officia nisi suscipit aliquam assumenda aperiam? Repellat quam ullam quasi asperiores. Laudantium omnis veritatis exercitationem. Harum doloribus, vero ut laudantium odit facilis veritatis ullam debitis accusamus dolor adipisci quae? Soluta, eaque optio at excepturi quasi, minima sit similique quisquam placeat omnis dolore. Temporibus iste ratione molestias placeat at exercitationem, corrupti accusantium unde iusto dolorum. ";
+$text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.Lorem ipsum dolor sit, amet consectetur adipisicing elit.  ";
+
+$parolaCensurata = $_GET["parola"];
+$carattereCensura = "***";
+$textCensurato = str_replace($parolaCensurata, $carattereCensura, $text);
+
+
 ?>
 
 
@@ -18,5 +24,12 @@ $text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci vero
 <body>
     
     <p> <?php echo $text?></p>
+    <h2> lunghezza paragrafo <?php echo strlen($text)?> lettere</h2>
+
+    <h1>paragrafo censurato</h1>
+    <p><?php echo $textCensurato?></p>
+    <h2> lunghezza paragrafo <?php echo strlen($textCensurato)?> lettere</h2>
+
+    
 </body>
 </html>
